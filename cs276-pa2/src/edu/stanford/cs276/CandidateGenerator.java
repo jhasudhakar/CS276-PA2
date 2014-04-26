cf0b3f31f00efe7bfdb73452f364b31444aa10c6
Wei Wei <fraternite@live.cn>
2014-04-26 14:11:50 -0700

    Edits1 added. Return all candidates within edit distance 1.

--- cs276-pa2/src/edu/stanford/cs276/CandidateGenerator.java
+++ cs276-pa2/src/edu/stanford/cs276/CandidateGenerator.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+import com.sun.javafx.geom.AreaOp;
 import edu.stanford.cs276.util.Pair;
 
 public class CandidateGenerator implements Serializable {
@@ -33,10 +34,81 @@ public class CandidateGenerator implements Serializable {
     public Set<String> getCandidates(String query) throws Exception {
         Set<String> candidates = new HashSet<String>();
         /*
-         * Your code here
+         * Test edit distance 1 first
          */
+        candidates.addAll(edits1(query));
         return candidates;
     }
 
+    /**
+     * Return all candidates whose edit distance are 1.
+     * @param word
+     * @return
+     */
+    private static Set<String> edits1(String word) {
+        Set<String> results = new HashSet<String>();
 
+        Set<Pair<String, String>> splits = new HashSet<Pair<String, String>>();
+        int length = word.length();
+        for (int i = 0; i <= length; i++) {
+            splits.add(new Pair(word.substring(0, i), word.substring(i)));
+        }
+
+        Set<String> deletes = new HashSet<String>();
+        for (Pair<String, String> p : splits) {
+            String w = p.getSecond();
+            if (w.length() >= 1) {
+                deletes.add(p.getFirst() + w.substring(1));
+            }
+        }
+
+        Set<String> inserts = new HashSet<String>();
+        for (Pair<String, String> p : splits) {
+            String w1 = p.getFirst();
+            String w2 = p.getSecond();
+            for (Character c : alphabet) {
+                inserts.add(w1 + c + w2);
+            }
+        }
+
+        Set<String> replaces = new HashSet<String>();
+        for (Pair<String, String> p : splits) {
+            String w1 = p.getFirst();
+            String w2 = p.getSecond();
+            if (w2.length() >= 1) {
+                for (Character c : alphabet) {
+                    replaces.add(w1 + c + w2.substring(1));
+                }
+            }
+        }
+
+        HashSet<String> transposes = new HashSet<String>();
+        for (Pair<String, String> p : splits) {
+            String w1 = p.getFirst();
+            String w2 = p.getSecond();
+            if (w2.length() > 1) {
+                transposes.add(w1 + w2.charAt(1) + w2.charAt(0) + w2.substring(2));
+            }
+        }
+
+        results.addAll(deletes);
+        results.addAll(inserts);
+        results.addAll(replaces);
+        results.addAll(transposes);
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        try {
+            CandidateGenerator cg = CandidateGenerator.get();
+            Set<String> candidates = cg.getCandidates("abcd");
+            for (String cand : candidates) {
+                System.out.println(cand);
+            }
+            System.out.println(candidates.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
