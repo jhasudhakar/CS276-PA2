0957e59196e256d3979812f06feec1b160da0a0f
Wei Wei <fraternite@live.cn>
2014-04-26 20:39:25 -0700

    add hyphen to alphabet

--- cs276-pa2/src/edu/stanford/cs276/CandidateGenerator.java
+++ cs276-pa2/src/edu/stanford/cs276/CandidateGenerator.java
@@ -28,7 +28,7 @@ public class CandidateGenerator implements Serializable {
                     'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
                     'o','p','q','r','s','t','u','v','w','x','y','z',
                     '0','1','2','3','4','5','6','7','8','9',
-                    ' ',','};
+                    ' ',',','-'};
 
     // Generate all candidates for the target query
     public Set<String> getCandidates(String query, Vocabulary vocabulary) throws Exception {
